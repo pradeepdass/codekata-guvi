969062bb845c650bd32e8517fef655fe4fc7f9ef
pradeepdass <smpradee10@gmail.com>
2017-01-24 13:06:14 +0530

    Create helllo.c

---
+++ helllo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int count;
+
+        for(count=1;count<=5;count++)
+        {
+            printf("Hello\n");
+        }
+	// your code goes here
+	return 0;
+}
+
