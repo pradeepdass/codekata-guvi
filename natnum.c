4acf1948e966b5fa91fe01d302d6c23ab076c1ba
pradeepdass <smpradee10@gmail.com>
2017-01-24 13:15:16 +0530

    Create natnum.c

---
+++ natnum.c
@@ -0,0 +1,11 @@
+int n, i, sum = 0;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    for(i=1; i <= n; ++i)
+    {
+        sum += i;   // sum = sum+i;
+    }
+
+    printf("Sum = %d",sum);
