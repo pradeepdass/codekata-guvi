b8672ac54f187bfda7ca82a8b1468c89fa56c9e9
pradeepdass <smpradee10@gmail.com>
2017-01-24 15:36:37 +0530

    Create pownum.c

---
+++ pownum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    int base, exponent;
+
+    long long result = 1;
+
+    printf("Enter a base number: ");
+    scanf("%d", &base);
+
+    printf("Enter an exponent: ");
+    scanf("%d", &exponent);
+
+    while (exponent != 0)
+    {
+        result *= base;
+        --exponent;
+    }
+
+    printf("Answer = %lld", result);
+
+    return 0;
+	// your code goes here
+	return 0;
+}
+
