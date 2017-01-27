6fb77a0a23766e9e487ff561911250153b0ef6aa
pradeepdass <smpradee10@gmail.com>
2017-01-27 10:48:18 +0530

    Create arm.c

---
+++ arm.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    int number, originalNumber, remainder, result = 0;
+
+    printf("Enter a three digit integer: ");
+    scanf("%d", &number);
+
+    originalNumber = number;
+
+    while (originalNumber != 0)
+    {
+        remainder = originalNumber%10;
+        result += remainder*remainder*remainder;
+        originalNumber /= 10;
+    }
+
+    if(result == number)
+        printf("%d is an Armstrong number.",number);
+    else
+        printf("%d is not an Armstrong number.",number);
+
+	// your code goes here
+	return 0;
+}
+
